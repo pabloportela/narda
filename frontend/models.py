7e1296c71eeb504987af49e0e1a52a8ede19b699
Pablo González Portela <pablo.portela@gmail.com>
2014-08-07 01:30:37 +0200

    Created frontend app and db model

---
+++ frontend/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+
+class User(models.Model):
+    username = models.CharField(max_length=200)
+    realname = models.CharField(max_length=200)
+    created_at = models.DateTimeField('date created')
+
+class Availability(models.Model):
+    chef = models.ForeignKey(User)
+    created_at = models.DateTimeField('date created')
+    available_at = models.DateTimeField('date of availability')
+    available_seats = models.IntegerField()
+    suggested_price = models.IntegerField()
+    is_available = models.IntegerField(default=1)
+
+class Inquiry(models.Model):
+    availability = models.ForeignKey(Availability)
+    guest = models.ForeignKey(User)
+    result = models.CharField(max_length=1) # accepted, rejected
+
+class InquiryText(models.Model):
+    inquiry = models.ForeignKey(Inquiry)
+    source = models.CharField(max_length=1) # guest, chef
+    text = models.CharField(max_length=255)
+    created_at = models.DateTimeField('date created')
+    type = models.CharField(max_length=1) # accept, reject, inquiry
+
+class Meal(models.Model):
+    created_at = models.DateTimeField('date created')
+    held_at = models.DateTimeField('date created')
+    chef = models.ForeignKey(User, related_name="chef")
+    guest = models.ForeignKey(User, related_name="guest")
+    cancelled_at = models.DateTimeField('date cancelled')
+    
+class Review(models.Model):
+    created_at = models.DateTimeField('date created')
+    guest = models.ForeignKey(User)
+    meal = models.ForeignKey(Meal)
+    text = models.CharField(max_length=255)
+
+class Menu(models.Model):
+    chef = models.ForeignKey(User)
+    text = models.CharField(max_length=255)
+
