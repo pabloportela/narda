28d40659025143d91015bf4d60051a1c6fca6142
Pablo González Portela <pablo.portela@gmail.com>
2014-08-07 18:10:29 +0200

    Played with the urls and the views.

--- frontend/views.py
+++ frontend/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Hello, world. You're at Narda.")
 
+def chef_detail(request, chef_id):
+    return HttpResponse("You're looking at chef %s." % chef_id)
+
--- narda/urls.py
+++ narda/urls.py
@@ -7,5 +7,9 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
+    
+    # eg. chef/45/
+    url(r'^chef/(?P<chef_id>\d+)/$',views.chef_detail,name='chef'),
+
     url(r'^loudadmin/', include(admin.site.urls)),
 )
