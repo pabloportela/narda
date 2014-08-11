5ca100020fcdf39de820105e2ace34ddf6095ccc
Pablo González Portela <pablo.portela@gmail.com>
2014-08-12 01:05:06 +0200

    Added python-social-auth, tested it with Google. Works!

--- frontend/views.py
+++ frontend/views.py
@@ -1,16 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 from django.http import HttpResponse
 from django.template import RequestContext, loader
 
 from frontend.models import Meal
 
 def index(request):
-    return render(request, 'index.html')
+    context = RequestContext(request, {'request': request, 'user': request.user})
+    return render_to_response('index.html', context_instance=context)
 
 def search(request, date):
     meal_list = Meal.objects.order_by('-scheduled_for')#[:5]
-    context = {'meal_list': meal_list, 'date': date}
-    return render(request, 'index.html', context)
+    context = RequestContext(request, {
+        'request': request,
+        'user': request.user,
+        'meal_list': meal_list,
+        'date': date
+    })
+    return render_to_response('index.html', context_instance=context)
 
 def chef_detail(request, chef_id):
     return HttpResponse("You're looking at chef %s." % chef_id)
--- narda/urls.py
+++ narda/urls.py
@@ -6,6 +6,10 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    url('', include('social.apps.django_app.urls', namespace='social')),
+    url('', include('django.contrib.auth.urls', namespace='auth')),
+    url(r'^social/', include('social.apps.django_app.urls', namespace='social')),
+
     url(r'^$', views.index, name='index'),
     
     # eg. chef/45/
@@ -15,4 +19,7 @@ urlpatterns = patterns('',
     url(r'^search/date/(?P<date>\d{4}-\d{2}-\d{2})/$',views.search,name='search'),
 
     url(r'^loudadmin/', include(admin.site.urls)),
+
+    # 3rd party authentication
+    url('', include('social.apps.django_app.urls', namespace='social')),
 )
