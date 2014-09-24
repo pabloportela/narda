280548ad3c6d8f96c3daa05d8e49a22b5bbe26ab
pabloportela <pablo.portela@booking.com>
2014-09-24 21:25:39 +0200

    Fixed what I had just broke on static files conf

--- narda/settings/base.py
+++ narda/settings/base.py
@@ -94,6 +94,13 @@ USE_L10N = True
 
 USE_TZ = True
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.6/howto/static-files/
+
+STATIC_URL = '/static/'
+
+STATIC_ROOT = join(SITE_ROOT, 'static')
+
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#template-dirs
 TEMPLATE_DIRS = (
