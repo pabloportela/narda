72215a954ab8054df4bb1bb39398c74e2aa0190e
Tayfun Sen <totayfun@gmail.com>
2014-10-20 23:14:34 +0200

    if no meal datetime is given, start from now

--- frontend/utils.py
+++ frontend/utils.py
@@ -12,7 +12,9 @@ def add_date(kwargs, given_date):
         2014-01-01/13-00
     etc.
     """
+    now = timezone.now()
     if not given_date:
+        kwargs["scheduled_for__gt"] = now
         return kwargs
     try:
         parsed_datetime = datetime.strptime(given_date, '%Y-%m-%d/%H/%M')
@@ -20,6 +22,10 @@ def add_date(kwargs, given_date):
             parsed_datetime,
             timezone.get_current_timezone(),
         )
+        # FIXME: it is better to redirect so that the user notices it is
+        # different now
+        # if parsed_datetime < now:
+        #     parsed_datetime = now
         kwargs['scheduled_for'] = parsed_datetime
         return kwargs
     except ValueError:
@@ -32,5 +38,11 @@ def add_date(kwargs, given_date):
         parsed_datetime,
         timezone.get_current_timezone(),
     )
+    # FIXME: it is better to redirect so that the user notices it is
+    # different now
+    # if parsed_datetime < now:
+    #     parsed_datetime = now
+        # kwargs['scheduled_for__gt'] = parsed_datetime
+        # return kwargs
     kwargs["scheduled_for__gt"] = parsed_datetime
     kwargs["scheduled_for__lt"] = parsed_datetime + timedelta(1)
